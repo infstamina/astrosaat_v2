b235b8acf9d120a698609a756b69f08f2c8f7681
Ayşen Unur <aysenunur@gmail.com>
2025-09-14 21:35:48 +0300

    PreDeploy

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App.jsx';
 import NextPage from './NextPage.jsx';
 import Today from './Today.jsx';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/next" element={<NextPage />} />
         <Route path="/today" element={<Today />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
